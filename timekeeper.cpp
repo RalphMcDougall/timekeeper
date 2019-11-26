3f8236fcbb794c742d70ef5285015775882be71c
Ralph McDougall <ralphmcdougall2000@gmail.com>
2019-11-26 22:04:54 +0200

    Added record keeper closure

--- timekeeper.cpp
+++ timekeeper.cpp
@@ -7,7 +7,9 @@ Tracker::Tracker(std::string _function_name)
 {
     // Constructor that starts timing of function
     function_name = _function_name;
-
+    
+    start_tp = std::chrono::high_resolution_clock::now();
+    RecordBook::function_name_set.insert(function_name);
 
     function_depth++;
 }
@@ -15,5 +17,23 @@ Tracker::Tracker(std::string _function_name)
 Tracker::~Tracker()
 {
     // Destructor that stops timing of function
+
+    auto finish_tp = std::chrono::high_resolution_clock::now();
+    tkll exec_time = std::chrono::duration_cast<std::chrono::nanoseconds>(finish_tp - start_tp).count();
+
+    RecordBook::function_records[function_name] += exec_time;
+
     function_depth--;
+
+    if (function_depth == 0)
+    {
+        // All functions exited and program ending
+
+        for (auto iter = RecordBook::function_name_set.begin(); iter != RecordBook::function_name_set.end(); ++iter)
+        {
+            // Output function executing times
+            std::string fn = *iter;
+            std::cout << fn << " : ("<< RecordBook::function_records[fn] << ")\n";
+        }
+    }
 }
--- timekeeper.h
+++ timekeeper.h
@@ -1,19 +1,35 @@
 #ifndef _TIMEKEEPER
 #define _TIMEKEEPER
 
+#include <iostream>
 #include <string>
+#include <unordered_map>
+#include <set>
+#include <chrono>
 
+typedef long long tkll;
 
 class Tracker
 {
     static int function_depth;
 
     std::string function_name;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_tp;
 
     public:
 
     Tracker(std::string function_name);
     ~Tracker();
+};
+
+
+class RecordBook
+{
+    public:
+    // Total execution times of all functions
+    static std::unordered_map<std::string, tkll> function_records;
+    static std::set<std::string> function_name_set;
+
 };
 
 
