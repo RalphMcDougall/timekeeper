80abaf08d5729a582843f818dbef281cd64a75a0
RalphMcDougall <ralphmcdougall2000@gmail.com>
2019-12-27 15:36:10 +0200

    Restructured timekeeping to make writing to CSV more natural

--- timekeeper.cpp
+++ timekeeper.cpp
@@ -1,43 +1,66 @@
 #include "timekeeper.h"
 
 // Set the initial function depth
-int Tracker::function_depth = 0;
+int RecordBook::function_depth = 0;
 
-std::unordered_map<std::string, tkll> RecordBook::function_records;
-std::set<std::string> RecordBook::function_name_set;
+std::vector<TKEvent*> RecordBook::event_order;
+int RecordBook::record_start;
+bool RecordBook::started = false;
+std::chrono::time_point<std::chrono::high_resolution_clock> RecordBook::ns_start_time;
+
+
+void RecordBook::addEvent(tkll event_time, EventType event_type, std::string event_issuer)
+{
+    TKEvent* ne = new TKEvent();
+    ne->time_stamp = event_time;
+    ne->event_type = event_type;
+    ne->event_issuer = event_issuer;
+
+    RecordBook::event_order.push_back(ne);
+}
 
 
 Tracker::Tracker(std::string _function_name)
 {
+    if (!RecordBook::started)
+    {
+        // First time a Tracker is being started
+        RecordBook::record_start = time(0);
+        RecordBook::started = true;
+        RecordBook::ns_start_time = std::chrono::high_resolution_clock::now();
+    }
+
     // Constructor that starts timing of function
     function_name = _function_name;
-    
-    start_tp = std::chrono::high_resolution_clock::now();
-    RecordBook::function_name_set.insert(function_name);
+    tkll exec_time = ( std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - RecordBook::ns_start_time) ).count();
 
-    function_depth++;
+    RecordBook::addEvent(exec_time, FUNCTION_START, function_name);
+
+    RecordBook::function_depth++;
 }
 
 Tracker::~Tracker()
 {
-    // Destructor that stops timing of function
-
+    // Destructor that indicates that function has ended
     auto finish_tp = std::chrono::high_resolution_clock::now();
-    tkll exec_time = std::chrono::duration_cast<std::chrono::nanoseconds>(finish_tp - start_tp).count();
+    tkll exec_time = std::chrono::duration_cast<std::chrono::nanoseconds>(finish_tp - RecordBook::ns_start_time).count();
 
-    RecordBook::function_records[function_name] += exec_time;
+    RecordBook::addEvent(exec_time, FUNCTION_END, function_name);
 
-    function_depth--;
-
-    if (function_depth == 0)
+    RecordBook::function_depth--;
+    if (RecordBook::function_depth == 0)
     {
         // All functions exited and program ending
+        // Create the CSV File for the report
+        std::string run_file = std::to_string(RecordBook::record_start) + ".csv";
+        std::ofstream csv_file(run_file, std::ofstream::out);
+        csv_file << "time,type,issuer\n";
 
-        for (auto iter = RecordBook::function_name_set.begin(); iter != RecordBook::function_name_set.end(); ++iter)
+        for (int i = 0; i < RecordBook::event_order.size(); ++i)
         {
-            // Output function executing times
-            std::string fn = *iter;
-            std::cout << fn << " : \t"<< RecordBook::function_records[fn] << "ns\n";
+            csv_file << RecordBook::event_order[i]->time_stamp << "," << RecordBook::event_order[i]->event_type << "," << RecordBook::event_order[i]->event_issuer << "\n";
+            free( RecordBook::event_order[i] );
         }
+        csv_file.close();
     }
 }
--- timekeeper.h
+++ timekeeper.h
@@ -1,35 +1,42 @@
 #ifndef _TIMEKEEPER
 #define _TIMEKEEPER
 
+#include <chrono>
+#include <fstream>
 #include <iostream>
+#include <set>
 #include <string>
 #include <unordered_map>
-#include <set>
-#include <chrono>
+#include <vector>
 
 typedef long long tkll;
 
+enum EventType {FUNCTION_START, FUNCTION_END};
 
-class RecordBook
+struct TKEvent
 {
-    public:
-    // Total execution times of all functions
-    static std::unordered_map<std::string, tkll> function_records;
-    static std::set<std::string> function_name_set;
+    tkll time_stamp;            // How many nanoseconds after the program started did this event get issued
+    EventType event_type;       // What is this event for
+    std::string event_issuer;   // Which function created the event
+};
+
+namespace RecordBook
+{
+    std::vector<TKEvent*> event_order;
+    int record_start;
+    bool started;
+    std::chrono::time_point<std::chrono::high_resolution_clock> ns_start_time;
+    int function_depth;
+
+    void addEvent(tkll, EventType, std::string);
 
 };
 
-
-
 class Tracker
 {
-    static int function_depth;
-
     std::string function_name;
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_tp;
 
     public:
-
     Tracker(std::string function_name);
     ~Tracker();
 };
