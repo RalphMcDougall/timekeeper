4cfce772a6d611b255b465e89f4140779477ab86
RalphMcDougall <ralphmcdougall2000@gmail.com>
2020-01-08 22:54:10 +0200

    Updated create_dist.sh

--- src/create_dist.sh
+++ src/create_dist.sh
@@ -3,4 +3,6 @@ g++ -c timekeeper.cpp -std=c++11 -O2
 echo "Copying header and object to distribution folder"
 cp timekeeper.o ../timekeeper
 cp timekeeper.h ../timekeeper
+echo "Copying analyzer to distribution folder"
+cp analyzer.py ../timekeeper
 echo "Done"
